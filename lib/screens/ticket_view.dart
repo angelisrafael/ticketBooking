1dbc84ce688ff74999dc27708ec88bedc6e1236a
RafaAngelis <r4f4518@gmail.com>
2023-01-04 15:15:50 -0300

    second commit

--- lib/screens/ticket_view.dart
+++ lib/screens/ticket_view.dart
@@ -5,6 +5,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:gap/gap.dart';
 
 class TicketView extends StatelessWidget {
   const TicketView({super.key});
@@ -17,7 +18,10 @@ class TicketView extends StatelessWidget {
       height: 200,
       child: Container(
         margin: const EdgeInsets.only(left: 16),
-        child: Column(children: [
+        child: Column(mainAxisSize: MainAxisSize.min, children: [
+          /*
+          showing the blue part of the card/tiket
+           */
           Container(
             decoration: const BoxDecoration(
                 color: Color(0xFF526799),
@@ -35,29 +39,133 @@ class TicketView extends StatelessWidget {
                   Expanded(child: Container()),
                   ThickContainer(),
                   Expanded(
-                      child: SizedBox(
-                    height: 24,
-                    child: LayoutBuilder(
-                      builder:
-                          (BuildContext context, BoxConstraints constraints) {
-                        print("The width is ${constraints.constrainWidth()}");
-                        return Flex(
-                          direction: Axis.horizontal,
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: List.generate(5, (index) => Text("-")),
-                        );
-                      },
-                    ),
+                      child: Stack(
+                    children: [
+                      SizedBox(
+                        height: 24,
+                        child: LayoutBuilder(
+                          builder: (BuildContext context,
+                              BoxConstraints constraints) {
+                            print(
+                                "The width is ${constraints.constrainWidth()}");
+                            return Flex(
+                              direction: Axis.horizontal,
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              mainAxisSize: MainAxisSize.max,
+                              children: List.generate(
+                                  (constraints.constrainWidth() / 6).floor(),
+                                  (index) => SizedBox(
+                                        width: 3,
+                                        height: 1,
+                                        child: DecoratedBox(
+                                            decoration: BoxDecoration(
+                                                color: Colors.white)),
+                                      )),
+                            );
+                          },
+                        ),
+                      ),
+                      Center(
+                        child: Transform.rotate(
+                          angle: 1.5,
+                          child: Icon(
+                            Icons.local_airport_rounded,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ],
                   )),
                   ThickContainer(),
                   Expanded(child: Container()),
                   Text(
-                    "London",
+                    "LDN",
                     style: Styles.headLineStyle3.copyWith(color: Colors.white),
                   )
                 ],
+              ),
+              const Gap(3),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  SizedBox(
+                    width: 100,
+                    child: Text(
+                      'New-York',
+                      style:
+                          Styles.headLineStyle4.copyWith(color: Colors.white),
+                    ),
+                  ),
+                  Text(
+                    '8H 30M',
+                    style: Styles.headLineStyle4.copyWith(color: Colors.white),
+                  ),
+                  SizedBox(
+                    width: 100,
+                    child: Text(
+                      'London',
+                      textAlign: TextAlign.end,
+                      style:
+                          Styles.headLineStyle4.copyWith(color: Colors.white),
+                    ),
+                  ),
+                ],
               )
             ]),
+          ),
+          /*
+          showing the orange part of the card/tiket
+           */
+          Container(
+            color: Styles.orangeColor, //const Color(0xFFF37B67),
+            child: Row(
+              children: [
+                const SizedBox(
+                  height: 20,
+                  width: 10,
+                  child: DecoratedBox(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(10),
+                        bottomRight: Radius.circular(10),
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(child: LayoutBuilder(
+                  builder: (BuildContext context, BoxConstraints constraints) {
+                    return Flex(
+                      direction: Axis.horizontal,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      mainAxisSize: MainAxisSize.max,
+                      children: List.generate(
+                          (constraints.constrainWidth()/15).floor(),
+                          (index) => const SizedBox(
+                                width: 5,
+                                height: 1,
+                                child: DecoratedBox(
+                                    decoration:
+                                        BoxDecoration(color: Colors.white)),
+                              )),
+                    );
+                  },
+                )),
+                SizedBox(
+                  height: 20,
+                  width: 10,
+                  child: DecoratedBox(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(10),
+                        bottomLeft: Radius.circular(10),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           )
         ]),
       ),
